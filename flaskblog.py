cf0b700c2e10a4fb5aa58a38435a819fb0c28f29
Mikica Mitrovic <bnmikica@gmail.com>
2021-08-16 18:49:07 +0200

    Part 4 - create database using Flask-SQLAlchemy

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,47 @@
+# enum Module and unique Class were imported automatically as I was typing code for class User
+from enum import unique
+
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+# This line was imported automatically as I was typing code for atribute posts in class User
+from sqlalchemy.orm import backref
+from wtforms.validators import Email
 from forms import RegistrationForm, LoginForm
 import jinja2
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'eb3f13aa289efe6c7325457574d3307c'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+# sqlite db simply will be the file on our file system. 
+# Class Models are SQL CLasses in SQLAlchemy.
+# User Model and Post Model are defined bellow.
+# Class Modeles represent Datebase Structure
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    # How to specify one to many relation between User and Post Models
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self) -> str:
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable = False)
+    date_posted = db.Column(db.DateTime, nullable = False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 posts = [
     {
