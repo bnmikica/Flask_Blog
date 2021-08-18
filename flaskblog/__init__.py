78f644d630245803ff7dbceff79f6741155d61b1
Mikica Mitrovic <bnmikica@gmail.com>
2021-08-18 20:54:26 +0200

    Part 6 - add/register users to database; login and logout users

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,10 +3,14 @@ from enum import unique
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+# Flask extensions like flask_bcrypt, flask_login
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_manager
 
 # This line was imported automatically as I was typing code for atribute posts in class User
 from sqlalchemy.orm import backref
 from wtforms.validators import Email
+# from wtforms.validators import DataRequired, Length, Email, EqualTo
 # This line is copied/pasted into routes.py file
 # from forms import RegistrationForm, LoginForm
 import jinja2
@@ -15,5 +19,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'eb3f13aa289efe6c7325457574d3307c'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
 
 from flaskblog import routes
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,17 +1,17 @@
 from datetime import datetime
-
-# N.B: A way to workaround following ImportError -->> 
-# ImportError: cannot import name 'User' from partially initialized module 'models' 
-# (most likely due to a circular import) (/home/bnmikica/Corey/Flask_Blog/models.py)
-# Original code: from flaskblog import db
-from flaskblog import db
+from flaskblog import db, login_manager
+from flask_login import UserMixin
 
 # sqlite db simply will be the file on our file system. 
 # Class Models are SQL CLasses in SQLAlchemy.
 # User Model and Post Model are defined bellow.
 # Class Modeles represent Datebase Structure
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
